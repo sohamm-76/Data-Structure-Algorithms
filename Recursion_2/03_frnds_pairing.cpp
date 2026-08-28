363c7d2fa3747bb9a0d6a975ce807381bfff0b9f
sohamm-76 <sohamm3176@gmail.com>
2026-08-28 19:32:14 +0530

    friends pairing problem in recursion

---
+++ Recursion_2/03_frnds_pairing.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int friendsPairing(int n){
+    if(n == 1 || n == 2){
+        return n;
+    }
+    
+    return friendsPairing(n-1) + (n-1) * friendsPairing(n-2); 
+}
+int main(){
+    cout << friendsPairing(3) << endl;//4 ways
+    return 0;
+}
